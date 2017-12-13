b6b25ee36bd270fadba7192b168bc8780ef55a1d
mrAppleXZ <mr.applexz@gmail.com>
2017-12-13 21:23:20 +0500

    Some work

---
+++ src/main/java/ru/pearx/libmc/client/ModelSupplied.java
@@ -0,0 +1,22 @@
+package ru.pearx.libmc.client;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import ru.pearx.lib.Supplied;
+
+import java.util.function.Supplier;
+
+/*
+ * Created by mrAppleXZ on 13.12.17 21:09.
+ */
+@SideOnly(Side.CLIENT)
+public class ModelSupplied extends Supplied<IBakedModel>
+{
+    public ModelSupplied(ModelResourceLocation loc)
+    {
+        super(() -> Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelShapes().getModelManager().getModel(loc));
+    }
+}
