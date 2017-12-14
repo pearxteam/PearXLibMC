577938a5fe57eca7e4b214255178d1fa85d735bd
mrAppleXZ <mr.applexz@gmail.com>
2017-12-14 20:28:35 +0500

    Updated all the libs

--- src/main/java/ru/pearx/libmc/common/blocks/BlockMultiblockPart.java
+++ src/main/java/ru/pearx/libmc/common/blocks/BlockMultiblockPart.java
@@ -142,6 +142,12 @@ public abstract class BlockMultiblockPart extends BlockBase
     }
 
     @Override
+    public int getPackedLightmapCoords(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        return super.getPackedLightmapCoords(state, source, pos);
+    }
+
+    @Override
     public EnumPushReaction getMobilityFlag(IBlockState state)
     {
         return EnumPushReaction.BLOCK;
